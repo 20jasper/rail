237c668132cd2d961eef6a2ce0e6e849b8265161
Jacob Asper <jacobasper191@gmail.com>
2025-01-23 19:44:50 -0500

    default integration test

--- tests/integration.rs
+++ tests/integration.rs
@@ -12,3 +12,12 @@ fn get_bytes() -> Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn defaults_to_ten() -> Result<()> {
+    let matches = commands::cli().get_matches_from(["rail", "tests/assets/test.txt"]);
+
+    assert_eq!(tail_file(matches)?, "3\n4\n5\n6\n7\n8\n9\n10\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n");
+
+    Ok(())
+}

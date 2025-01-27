a9d616eea3cabb5961a06132cb58fefe4a5c4c2a
Jacob Asper <jacobasper191@gmail.com>
2025-01-27 00:37:39 -0500

    update write to file helper

--- src/bin/write_to_file.rs
+++ src/bin/write_to_file.rs
@@ -1,16 +1,35 @@
 use core::error::Error;
 use core::time::Duration;
+use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::thread::sleep;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut f = File::options().append(true).open("test.txt")?;
+    let mut args = env::args();
+    args.next();
+    let path = args.next().unwrap_or("test.txt".to_owned());
+    let lines = args.next().unwrap_or("true".to_owned()).parse::<bool>()?;
 
-    for i in 0..10 {
-        writeln!(f, "{i}")?;
-        sleep(Duration::from_secs(1));
+    let mut f = File::options().append(true).open(path)?;
+
+    if lines {
+        for i in 0..10 {
+            writeln!(f, "{i}")?;
+            sleep(Duration::from_millis(500));
+            println!("wriet lines");
+        }
+        println!("done with lines");
+    } else {
+        for _ in 0..10 {
+            write!(&mut f, "hi")?;
+            f.flush()?;
+            sleep(Duration::from_millis(500));
+            println!("wriet char");
+        }
     }
+
+    println!("done");
 
     Ok(())
 }
